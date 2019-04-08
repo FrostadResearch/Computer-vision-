5bca40653d7226c91899f632b6c15de8b2a581ee
FrostadResearch <frostadresearch@gmail.com>
2019-04-08 03:56:25 -0700

    version 3
    Cleaned up code, makes pretty file now, checks darkness of images. Very cool :)

--- counter2.1.py
+++ counter2.1.py
@@ -4,28 +4,8 @@ import csv
 import numpy as np
 import cProfile
 import math
+import imageio
 
-
-##def process(i):
-##    
-##    #import image
-##    img = cv2.imread(i)
-##
-##    #modify image 
-##    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-##
-##    im, thresh = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY_INV)
-##
-##    #blur to make contour detection easier
-##    thresh = cv2.GaussianBlur(thresh, (9,9), 0)
-##    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-##    cv2.resizeWindow('img', 1000, 750)
-##
-##    #find contours
-##    ctrs, im = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-##    cv2.drawContours(img, ctrs, -1, (0,255,0),2)
-##    cv2.imshow("img", img)
-##    return ctrs
 
 def process(i):
     
@@ -56,7 +36,7 @@ def process(i):
             continue
         ellipse = cv2.fitEllipse(cnt)
         cv2.drawContours(img, [cnt], 0, (0,255,0),2)
-        #cv2.ellipse(img, ellipse, (0,255,0), 2)
+        
     
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('img', 1000, 750)
@@ -97,7 +77,6 @@ def process_small(i, x, y):
             continue
         ellipse = cv2.fitEllipse(cnt)
         cv2.drawContours(img, [cnt], 0, (0,255,0),2)
-        #cv2.ellipse(img, ellipse, (0,255,0), 2)
     
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('img', 1000, 750)
@@ -105,6 +84,180 @@ def process_small(i, x, y):
  
     cv2.imshow("img", img)
     return contours
+
+def image_light(fn):
+    img = imageio.imread(fn, as_gray=True)
+    return np.mean(img)
+
+def good_cont(c):
+    return cv2.contourArea(c)>1000 and cv2.contourArea(c)<10000
+
+def pick_cells(i):
+    ctrs = process(i)
+    img = cv2.imread(i)
+
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('img', 1000, 750)
+
+    #iterate over list of contours to only draw and count valid contours 
+    i=0
+    #cellCount = 0
+    while (i< len(ctrs)):
+        if (good_cont(ctrs[i])):
+            print(i)
+
+            #draw and count valid contours
+            cv2.drawContours(img, ctrs, i, (0,255,0),2)  
+            #cellCount= cellCount + 1        
+
+            # label contour on image 
+            extRight = tuple(ctrs[i][ctrs[i][:, :, 0].argmax()][0])
+            cv2.circle(img, extRight, 8, (255, 0, 0), -1)
+            cv2.putText(img,str(i), extRight,
+                        cv2.FONT_HERSHEY_TRIPLEX, 2, 255,4)
+        
+        i= i + 1
+        
+    print("choose a cell to track:")
+    
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('img', 1000, 750)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cell_str = input()
+    cell = int(cell_str)
+    print("thanks!")
+
+    #just me testing to make sure the correct contour is being picked 
+    cv2.drawContours(img, ctrs, cell, (0,0,255),2)
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('img', 1000, 750)
+    #test get centre functions 
+    x = get_centre_val(ctrs[cell],"x")
+    y = get_centre_val(ctrs[cell],"y")
+    cv2.putText(img,"the point", (x,y),
+                        cv2.FONT_HERSHEY_TRIPLEX, 2, (0,0,255),4)
+    cv2.imshow("img", img)
+    #end testing 
+    
+    return ctrs[cell]
+   
+    
+def get_data(c):
+    return cv2.contourArea(c)
+
+
+def get_centre_val(c,xy):
+    x, y, w, h = cv2.boundingRect(c)
+    cx = x
+    cy = y
+    if xy == "x":
+        return int(cx)
+    else:
+        return int(cy)
+    
+
+def distance_from_centre(c):
+    #based off of crop size in process_small
+    x = abs(get_centre_val(c, "x") - 500) 
+    y = abs(get_centre_val(c, "y") - 500)
+    return x+y
+
+
+def get_best_ctr(ctrs):
+    closest= ctrs[0]
+    for c in ctrs:
+        if distance_from_centre(c)< distance_from_centre(closest):
+            closest = c
+    return closest
+
+def main(img):
+
+    init = pick_cells(img)
+    initTime = int(img[-19:-6])
+    files = glob.glob('2019-03-01_20;0*.jpg')
+    nfiles = len(files)
+
+    last = init
+    cellY = get_centre_val(last, "y")
+    cellX = get_centre_val(last, "x")
+    print(cellX)
+    print(cellY)
+    i = 0
+    
+
+    with open('cell Data file', 'w') as csvfile:
+        wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        wr.writerow(["time", "cell size"])
+    
+        for file in files:
+           if image_light(file)< 90:
+               return csv
+           ctrs = process_small(file, cellX,cellY)
+           best= get_best_ctr(ctrs)
+           print(get_data(best))
+           cellData = get_data(best)
+           last = best
+           time = int(file[-19:-6])
+           row = [time, cellData]
+           wr.writerow(row)
+
+
+           i += 1
+           if i == 650:
+               return csv
+    
+    return csv
+
+ 
+    
+
+main("2019-03-01_20;01_1551499548124ms.jpg")
+        
+def profile():
+    cProfile.run('main("2019-03-01_20;01_1551499548124ms.jpg")')
+
+
+    
+
+## USELESS FUNCTIONS
+##--------------------------------------------------------------------
+def reasonable(c, e):
+    return get_data(c) > get_data(e) - 50 #and get_data(c) < get_data(e) + 100 
+
+def good_cell(i, c):
+    diff = 10
+    iCell = get_centre(i)
+    cCell = get_centre(i)
+    x = abs(iCell[0] - cCell[0])
+    y = abs(iCell[1] - cCell[1])
+    return x < diff and y < diff    
+
+def get_centre(c):
+    x, y, w, h = cv2.boundingRect(c)
+    cx = x
+    cy = y
+    return [cx,cy]
+
+def make_file(locd):
+    with open('cell Data file', 'w') as csvfile:
+        wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        wr.writerow(locd)
+        # I realize that this adds this all in one row
+
+
+#not actually callable, just storing my code fragment
+#so i dont have to look for it later
+def test():
+    #start testing
+    img = process_small_test(file, cellX, cellY)
+    cv2.drawContours(img, best, -1, (0,0,255),2)
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('img', 1000, 750)
+    cv2.imshow("img", img)
+    #end testing
 
 def process_small_test(i, x, y):
     
@@ -146,166 +299,23 @@ def process_small_test(i, x, y):
     cv2.imshow("img", img)
     return img
 
-def good_cont(c):
-    return cv2.contourArea(c)>1000 and cv2.contourArea(c)<10000
-
-def pick_cells(i):
-    ctrs = process(i)
-    img = cv2.imread(i)
-
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('img', 1000, 750)
-
-    #find contours
-    #cv2.drawContours(img, ctrs, -1, (0,255,0),2)
-    #cv2.imshow("img", img)
-
-
-    #iterate over list of contours to only draw and count valid contours 
-    i=0
-    #cellCount = 0
-    while (i< len(ctrs)):
-        if (good_cont(ctrs[i])):
-            print(i)
-
-            #draw and count valid contours
-            cv2.drawContours(img, ctrs, i, (0,255,0),2)  
-            #cellCount= cellCount + 1        
-
-            # label contour on image 
-            extRight = tuple(ctrs[i][ctrs[i][:, :, 0].argmax()][0])
-            cv2.circle(img, extRight, 8, (255, 0, 0), -1)
-            cv2.putText(img,str(i), extRight,
-                        cv2.FONT_HERSHEY_TRIPLEX, 2, 255,4)
-        
-        i= i + 1
-        
-    print("choose a cell to track:")
-    
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('img', 1000, 750)
-    cv2.imshow("img", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    cell_str = input()
-    cell = int(cell_str)
-    print("thanks!")
-
-    #check to make sure the correct contour is being picked 
-    cv2.drawContours(img, ctrs, cell, (0,0,255),2)
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('img', 1000, 750)
-    #test get centre functions 
-    x = get_centre_val(ctrs[cell],"x")
-    y = get_centre_val(ctrs[cell],"y")
-    cv2.putText(img,"the point", (x,y),
-                        cv2.FONT_HERSHEY_TRIPLEX, 2, (0,0,255),4)
-    cv2.imshow("img", img)
-    
-    return ctrs[cell]
-   
-    
-def get_data(c):
-    return cv2.contourArea(c)
-
-def get_centre(c):
-    x, y, w, h = cv2.boundingRect(c)
-    cx = x
-    cy = y
-    return [cx,cy]
-
-def get_centre_val(c,xy):
-    x, y, w, h = cv2.boundingRect(c)
-    cx = x
-    cy = y
-    if xy == "x":
-        return int(cx)
-    else:
-        return int(cy)
-    
-
-def good_cell(i, c):
-    diff = 10
-    iCell = get_centre(i)
-    cCell = get_centre(i)
-    x = abs(iCell[0] - cCell[0])
-    y = abs(iCell[1] - cCell[1])
-    return x < diff and y < diff
-
-def distance_from_centre(c):
-    #based off of crop size in process_small
-    x = abs(get_centre_val(c, "x") - 500) 
-    y = abs(get_centre_val(c, "y") - 500)
-    return x+y
-
-def make_file(locd):
-    with open('cell Data file', 'w') as csvfile:
-        wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        wr.writerow(locd)
-        # I realize that this adds this all in one row
-
-def reasonable(c, e):
-    return get_data(c) > get_data(e) - 50 #and get_data(c) < get_data(e) + 100 
-
-def get_best_ctr(ctrs):
-    closest= ctrs[0]
-    for c in ctrs:
-        if distance_from_centre(c)< distance_from_centre(closest):
-            closest = c
-    return closest
-
-def main(img):
-
-    init = pick_cells(img)
-    initTime = int(img[-19:-6])
-    files = glob.glob('2019-03-01_20;0*.jpg')
-    nfiles = len(files)
-
-    last = init
-    cellY = get_centre_val(last, "y")
-    cellX = get_centre_val(last, "x")
-    print(cellX)
-    print(cellY)
-    i = 0
-    
-
-    with open('cell Data file', 'w') as csvfile:
-        wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        wr.writerow(["time", "cell size"])
-    
-        for file in files:
-           ctrs = process_small(file, cellX,cellY)
-           best= get_best_ctr(ctrs)
-           print(get_data(best))
-           cellData = get_data(best)
-           last = best
-           time = int(file[-19:-6])
-           row = [time, cellData]
-           wr.writerow(row)
-
-
-           i += 1
-           if i ==100:
-               return csv
-    
-    if i ==100:
-        
-        return csv
-
- 
-    
-
-main("2019-03-01_20;01_1551499548124ms.jpg")
-        
-def profile():
-    cProfile.run('main("2019-03-01_20;01_1551499548124ms.jpg")')
-
-def test():
-    #start testing
-    img = process_small_test(file, cellX, cellY)
-    cv2.drawContours(img, best, -1, (0,0,255),2)
-    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('img', 1000, 750)
-    cv2.imshow("img", img)
-    #end testing
+##def process(i):
+##    
+##    #import image
+##    img = cv2.imread(i)
+##
+##    #modify image 
+##    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+##
+##    im, thresh = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY_INV)
+##
+##    #blur to make contour detection easier
+##    thresh = cv2.GaussianBlur(thresh, (9,9), 0)
+##    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+##    cv2.resizeWindow('img', 1000, 750)
+##
+##    #find contours
+##    ctrs, im = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+##    cv2.drawContours(img, ctrs, -1, (0,255,0),2)
+##    cv2.imshow("img", img)
+##    return ctrs
